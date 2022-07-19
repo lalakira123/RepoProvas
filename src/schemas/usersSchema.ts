1d571e053cca2cb19b2c4e4463f33f0c7ddd8639
Laerte <laerteakira@gmail.com>
2022-07-19 13:01:19 -0300

    refactor: interface to usersSchema

--- src/schemas/usersSchema.ts
+++ src/schemas/usersSchema.ts
@@ -1,6 +1,12 @@
 import Joi from 'joi';
 
-export const usersSchema = Joi.object({
+interface User {
+  email: string,
+  password: string,
+  confirmPassword: string
+}
+
+export const usersSchema = Joi.object<User>({
   email: Joi.string().email().required(),
   password: Joi.string().required(),
   confirmPassword: Joi.string().valid(Joi.ref('password')).required()
