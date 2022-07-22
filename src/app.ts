a0b82824c53d4158d1f8b9ba6f4d3ea11ef377e4
Laerte <laerteakira@gmail.com>
2022-07-21 21:25:17 -0300

    feat: setup tests && seed

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,16 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import router from './routers/index.js';
 
 import handleError from './middlewares/errorHandlerMiddleware.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
-dotenv.config();
 
 app.use(cors());
 app.use(json());
 app.use(router);
 app.use(handleError);
 
-app.listen(port, () => {
-  console.log(`Server is running on PORT: ${port}`);
-});
+export default app;
---
+++ tests/tests.test.ts
@@ -0,0 +1,4 @@
+import app from './../src/app.js';
+import supertest from 'supertest';
+
+import { prisma } from './../src/config/db.js';
